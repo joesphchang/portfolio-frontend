031604c333511b6efe353418cd166849234f06e7
joesphchang <joesphschang@gmail.com>
2022-02-24 13:14:27 -0500

    Added details to portfolio component

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,30 +1,37 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React from 'react';
+import './Portfolio.css';
+import { useEffect, useState } from 'react';
 
 function Portfolio() {
-    const [details, setDetails] = useState([]);
+	const [details, setDetails] = useState([]);
 
-    useEffect(() => {
-        fetch('https://jojos-portfolio.herokuapp.com/api/portfolios')
-            .then((res) => res.json())
-            .then((res) => {
-                setDetails(res);
-            });
-    }, []);
-  return (
-    <div>
-        <h2>Projects</h2>
-        <ul>
-            {details.map((detail) => {
-                return (
-                    <li>
-                        <img src={detail.image} alt='project info' />
-                    </li>
-                )
-            })}
-        </ul>
-    </div>
-  )
+	useEffect(() => {
+		fetch('https://jojos-portfolio.herokuapp.com/api/portfolios')
+			.then((res) => res.json())
+			.then((res) => {
+				setDetails(res);
+			});
+	}, []);
+	return (
+		<div className='detail-container'>
+			<h2>Projects</h2>
+			<div className='card-container'>
+				<ul className='card-list'>
+					{details.map((detail) => {
+						return (
+							<li className='card-item'>
+								<img src={detail.image} alt='project info' />
+								<h3>{detail.title}</h3>
+								<p>{detail.description}</p>
+								<a href={detail.deployed_site}>Deployed Site</a>
+                                <a href={detail.github}>Github</a>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
-export default Portfolio
+export default Portfolio;
