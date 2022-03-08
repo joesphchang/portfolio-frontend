00cf598a2d020bf9d54884a61bf6113002de1ab6
joesphchang <joesphschang@gmail.com>
2022-03-08 17:05:48 -0500

    finalized changes to home

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import BackgroundVideo from '../../assets/red-drapes.mp4';
+import backgroundImage from '../../assets/hero-image.png';
 import './Hero.css';
 
 function Hero() {
 	return (
 		<div className='hero' id='home'>
-			<video autoPlay loop muted className='hero-bg'>
-				<source src={BackgroundVideo} type='video/mp4'></source>
-			</video>
+			<div>
+				<img src={backgroundImage} alt='background' />
+			</div>
 			<div className='content'>
 				<h1>Joey Chang</h1>
 			</div>
