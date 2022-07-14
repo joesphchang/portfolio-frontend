32acfb41d9ddb6a3eb67a6cadea8e2fe2d3b5356
joesphchang <joesphschang@gmail.com>
2022-07-14 16:10:36 -0400

    finished hero-page desktop view

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,17 +4,20 @@ import './Hero.css';
 function Hero() {
 	return (
 		<div className='hero' id='home'>
-			<div className='ellipse'></div>
 			<div className='content'>
-				<h1 className='hero-main-header'>Hi, I am Joey Chang</h1>
-				<h2 className='hero-sub-header'>Front End Developer</h2>
-			</div>
+				<div className='ellipse'></div>
+				<div>
+					<h1 className='hero-main-header'>Hi, I am Joey Chang</h1>
+					<h2 className='hero-sub-header'>Front End Developer</h2>
+				</div>
+				
 			<div className='right-content'>
 				<p className='hero-main-paragraph'>
 					I create inclusive, accessible digital websites. I believe in using
 					web-developing as a tool to elevate human interaction with technology
 					that scale.
 				</p>
+			</div>
 			</div>
 		</div>
 	);
